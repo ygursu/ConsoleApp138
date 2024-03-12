24d3900f5a7117aabd6b5e7ef83d5e95cf7c41d4
ygursu <yasemingrs@gmail.com>
2024-03-12 23:09:50 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp138/Program.cs
@@ -0,0 +1,28 @@
+﻿double fiyat = 0;
+int adet = 0;
+Console.Write("Ürünün fiyatını giriniz:");
+
+OkuFiyat:
+fiyat = Convert.ToDouble(Console.ReadLine());
+
+if (fiyat <= 0)
+{
+    Console.Write("Yanlış fiyat bilgisi girdiniz.\nFiyatı tekrardan giriniz:");
+    goto OkuFiyat;
+}
+
+
+Console.Write("Aldığınız ürün miktarını giriniz:");
+
+OkuAdet:
+adet = Convert.ToInt32(Console.ReadLine());
+
+if (adet <= 0)
+{
+    Console.Write("Yanlış miktar bilgisi girdiniz.\nAldığınız ürün miktarını tekrardan giriniz:");
+    goto OkuAdet;
+}
+
+double tutar = fiyat * adet;
+Console.WriteLine("Ödeyeceğiniz toplam tutar {0}TL", tutar);
+Console.ReadKey();
